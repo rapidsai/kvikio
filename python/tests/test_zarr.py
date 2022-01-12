4cff81cd6dec10051c39d9ce8e01d5d05f719e8a
Mads R. B. Kristensen <madsbk@gmail.com>
2022-01-12 13:27:57 +0100

    Adding test of Zarr Array and Group
    This requires https://github.com/zarr-developers/zarr-python/pull/934

--- python/tests/test_zarr.py
+++ python/tests/test_zarr.py
@@ -27,3 +27,45 @@ def test_direct_store_access(store, array_type):
     # Notice, GDSStore always returns a cupy array
     assert type(b) is cupy.ndarray
     cupy.testing.assert_array_equal(a, b)
+
+
+def test_array(store):
+    """Test Zarr array"""
+
+    pytest.importorskip(
+        "zarr.cupy",
+        reason=(
+            "To use Zarr arrays with GDS directly, Zarr needs CuPy support: "
+            "<https://github.com/zarr-developers/zarr-python/pull/934>"
+        ),
+    )
+
+    a = cupy.arange(100)
+    z = zarr.array(
+        a, chunks=10, compressor=None, store=store, meta_array=cupy.empty(())
+    )
+    assert a.shape == z.shape
+    assert a.dtype == z.dtype
+    assert isinstance(a, type(z[:]))
+    cupy.testing.assert_array_equal(a, z[:])
+
+
+def test_group(store):
+    """Test Zarr group"""
+
+    pytest.importorskip(
+        "zarr.cupy",
+        reason=(
+            "To use Zarr arrays with GDS directly, Zarr needs CuPy support: "
+            "<https://github.com/zarr-developers/zarr-python/pull/934>"
+        ),
+    )
+
+    g = zarr.open_group(store, meta_array=cupy.empty(()))
+    g.ones("data", shape=(10, 11), dtype=int, compressor=None)
+    a = g["data"]
+    assert a.shape == (10, 11)
+    assert a.dtype == int
+    assert isinstance(a, zarr.Array)
+    assert isinstance(a[:], cupy.ndarray)
+    assert (a[:] == 1).all()
