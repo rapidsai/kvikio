b7ae6cc83d74e3a7052e216b544f0bb978b39373
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-02-10 06:46:36 -0800

    Make sure make command executes after cmake

--- conda/recipes/libkvikio/build.sh
+++ conda/recipes/libkvikio/build.sh
@@ -1,8 +1,6 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
-cd cpp
-mkdir build
-cd build
-cmake ..
-make
+mkdir cpp/build
+cd cpp/build
+cmake .. && make
