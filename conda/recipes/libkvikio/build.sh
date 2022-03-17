2135577befe995c84d656cf9f434ffab69933499
John Kirkham <jakirkham@gmail.com>
2022-03-16 22:40:55 -0700

    Configure CMake to install in `$PREFIX`

--- conda/recipes/libkvikio/build.sh
+++ conda/recipes/libkvikio/build.sh
@@ -3,6 +3,8 @@
 # This assumes the script is executed from the root of the repo directory
 mkdir cpp/build
 cd cpp/build
-cmake ..
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+
 make
 make install
