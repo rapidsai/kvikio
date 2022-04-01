7a43759ae8638c17a02f68f4e8d777e52480fe37
AJ Schmidt <aschmidt@nvidia.com>
2022-04-01 09:43:07 -0400

    DOC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = '2022, NVIDIA'
 author = 'NVIDIA'
 
 # The short X.Y version.
-version = '22.04'
+version = '22.06'
 # The full version, including alpha/beta/rc tags
-release = '22.04.00'
+release = '22.06.00'
 
 
 # -- General configuration ---------------------------------------------------
