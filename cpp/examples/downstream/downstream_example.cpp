60340642f83ed1db56dcb3a2252ad692b597fc78
Mads R. B. Kristensen <madsbk@gmail.com>
2022-06-13 08:33:44 +0200

    clean up

--- cpp/examples/downstream/downstream_example.cpp
+++ cpp/examples/downstream/downstream_example.cpp
@@ -1,32 +1,28 @@
-/*
- * Copyright (c) 2021-2022, NVIDIA CORPORATION.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <iostream>
 
+#include <cuda_runtime_api.h>
+
 #include <kvikio/buffer.hpp>
-#include <kvikio/config.hpp>
+#include <kvikio/defaults.hpp>
 #include <kvikio/driver.hpp>
 #include <kvikio/file_handle.hpp>
 
 using namespace std;
 
+void check(bool condition)
+{
+  if (!condition) {
+    std::cout << "Error" << std::endl;
+    exit(-1);
+  }
+}
+
 int main()
 {
-  cout << "KvikIO config: " << endl;
-  if (kvikio::config::get_global_compat_mode()) {
+  check(cudaSetDevice(0) == cudaSuccess);
+
+  cout << "KvikIO defaults: " << endl;
+  if (kvikio::defaults::compat_mode()) {
     cout << "  Compatibility mode: enabled" << endl;
   } else {
     kvikio::DriverInitializer manual_init_driver;
