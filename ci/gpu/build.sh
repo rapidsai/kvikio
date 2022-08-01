dac32a14b55c51abb456ba2066754e348ff2cf5b
Mads R. B. Kristensen <madsbk@gmail.com>
2022-08-01 16:35:08 +0200

    CI: build without cuFile available

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -83,6 +83,14 @@ fi
 # Run basic_io
 ${WORKSPACE}/libkvikio-debug-build/examples/basic_io
 
+gpuci_logger "Build and run libkvikio-no-cufile"
+mkdir "${WORKSPACE}/libkvikio-no-cufile-build"
+cd "${WORKSPACE}/libkvikio-no-cufile"
+cmake ${WORKSPACE}/cpp -DCMAKE_DISABLE_FIND_PACKAGE_cuFile=TRUE
+make
+# Run basic_io
+${WORKSPACE}/libkvikio-no-cufile/examples/basic_io
+
 cd "${WORKSPACE}/python"
 gpuci_logger "Python py.test for kvikio"
 py.test --cache-clear --basetemp="${WORKSPACE}/cudf-cuda-tmp" --junitxml="${WORKSPACE}/junit-kvikio.xml" -v
