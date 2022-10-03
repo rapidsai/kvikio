890d9d6a977d08994f6b2dc2ff5e4d427c7bbe3f
Mads R. B. Kristensen <madsbk@gmail.com>
2022-10-03 15:33:21 +0200

    Impl. FromFile

---
+++ python/kvikio/overload.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+# See file LICENSE for terms.
+
+import os.path
+
+import kvikio
+
+
+class FromFile:
+    def __init__(self, meta_array) -> None:
+        self._meta_array = meta_array
+
+    def __array_function__(self, func, types, args, kwargs):
+        import numpy
+
+        if func is not numpy.fromfile:
+            raise NotImplementedError()
+
+        if len(args):
+            filepath = args[0]
+            args = args[1:]
+        else:
+            filepath = kwargs.pop("file")
+
+        if len(args):
+            dtype = args[0]
+            args = args[1:]
+        else:
+            dtype = kwargs.pop("dtype", float)
+
+        if args or kwargs:
+            raise NotImplementedError("only implements the file and dtype arguments")
+
+        nbytes = os.path.getsize(filepath)
+        itemsize = numpy.dtype(dtype).itemsize
+        size = nbytes // itemsize
+        if nbytes % itemsize != 0:
+            raise ValueError(
+                f"file size ({nbytes}) not divisible with dtype size ({itemsize})"
+            )
+
+        ret = numpy.empty_like(self._meta_array, shape=(size,), dtype=dtype)
+        with kvikio.CuFile(filepath, "r") as f:
+            f.read(ret)
+
+        return ret
---
+++ python/tests/test_overload.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2021-2022, NVIDIA CORPORATION. All rights reserved.
+# See file LICENSE for terms.
+
+
+import pytest
+
+from kvikio.overload import FromFile
+
+
+@pytest.mark.parametrize("xp", ["numpy", "cupy"])
+@pytest.mark.parametrize("dtype", ["u1", "int64", "float32", "float64"])
+def test_from_file(tmp_path, xp, dtype):
+    """Test NumPy's and CuPy's fromfile() with FromFile"""
+
+    xp = pytest.importorskip(xp)
+    filepath = str(tmp_path / "test_from_file")
+    src = xp.arange(100, dtype=dtype)
+    src.tofile(filepath)
+    like = FromFile(meta_array=xp.empty(()))
+    dst = xp.fromfile(filepath, dtype, like=like)
+    xp.testing.assert_array_equal(src, dst)
+    dst = xp.fromfile(filepath, dtype=dtype, like=like)
+    xp.testing.assert_array_equal(src, dst)
+    dst = xp.fromfile(file=filepath, dtype=dtype, like=like)
+    xp.testing.assert_array_equal(src, dst)
+
+
+@pytest.mark.parametrize("xp", ["numpy", "cupy"])
+def test_from_file_error(tmp_path, xp):
+    xp = pytest.importorskip(xp)
+    filepath = str(tmp_path / "test_from_file")
+    src = xp.arange(1, dtype="u1")
+    src.tofile(filepath)
+
+    with pytest.raises(FileNotFoundError, match="no file"):
+        xp.fromfile("no file", like=FromFile(meta_array=src))
+
+    with pytest.raises(ValueError, match="not divisible with dtype"):
+        xp.fromfile(file=filepath, like=FromFile(meta_array=src))
