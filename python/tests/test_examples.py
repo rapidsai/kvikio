e4667f77114565bc5e4e359e55895cd2e3a8375b
Mads R. B. Kristensen <madsbk@gmail.com>
2022-10-04 10:01:53 +0200

    test_hello_world: require cupy

--- python/tests/test_examples.py
+++ python/tests/test_examples.py
@@ -5,10 +5,14 @@ import os
 from importlib import import_module
 from pathlib import Path
 
+import pytest
+
 examples_path = Path(os.path.realpath(__file__)).parent / ".." / "examples"
 
 
 def test_hello_world(tmp_path, monkeypatch):
     """Test examples/hello_world.py"""
+    pytest.importorskip("cupy")
+
     monkeypatch.syspath_prepend(str(examples_path))
     import_module("hello_world").main(tmp_path / "test-file")
