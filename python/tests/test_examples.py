93604d7d9409cb337f45aef3076307dcf018c55b
Mads R. B. Kristensen <madsbk@gmail.com>
2022-10-07 11:21:12 +0200

    comment

--- python/tests/test_examples.py
+++ python/tests/test_examples.py
@@ -12,7 +12,7 @@ examples_path = Path(os.path.realpath(__file__)).parent / ".." / "examples"
 
 def test_hello_world(tmp_path, monkeypatch):
     """Test examples/hello_world.py"""
-    pytest.importorskip("cupy")
+    pytest.importorskip("cupy")  # `examples/hello_world.py` requires CuPy
 
     monkeypatch.syspath_prepend(str(examples_path))
     import_module("hello_world").main(tmp_path / "test-file")
