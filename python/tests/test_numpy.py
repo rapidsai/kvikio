7b4bee71147e6cc356ab1e3944fdb97071703605
Mads R. B. Kristensen <madsbk@gmail.com>
2022-10-12 18:24:31 +0200

    Overload `numpy.fromfile()` and `cupy.fromfile()` (#135)
    This PR introduce `LikeWrapper`, which can be given to `numpy.fromfile()` and `cupy.fromfile()` to utilize KvikIO and GDS (if available).
    
    In the following, we load `"my-file.data"` as a NumPy and a CuPy array.
    ```python
    import numpy as np
    import cupy as cp
    from kvikio.numpy import LikeWrapper
    
    a = np.fromfile("my-file.data", like=LikeWrapper(np.empty(())))
    b = cp.fromfile("my-file.data", like=LikeWrapper(cp.empty(())))
    ```
    
    This PR also introduces `fromfile()`, which is a drop-in replacement for NumPy and CuPy's `fromfile()`:
    ```python
    import numpy as np
    import cupy as cp
    from kvikio.numpy import fromfile
    
    a = fromfile("my-file.data", like=np.empty(()))
    b = fromfile("my-file.data", like=cp.empty(()))
    ```
    
    Finally, we have `tofile()` as an alternative to NumPy and CuPy's `.tofile()` method:
    ```python
    import numpy as np
    import cupy as cp
    from kvikio.numpy import tofile
    
    tofile(np.arange(10), "my-file.data")
    tofile(cp.arange(10), "my-file.data")
    ```
    
    xref: https://github.com/cupy/cupy/issues/2576
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Benjamin Zaitlen (https://github.com/quasiben)
    
    URL: https://github.com/rapidsai/kvikio/pull/135

---
+++ python/tests/test_numpy.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2021-2022, NVIDIA CORPORATION. All rights reserved.
+# See file LICENSE for terms.
+
+
+import pytest
+
+from kvikio.numpy import LikeWrapper, tofile
+
+
+@pytest.mark.parametrize("xp", ["numpy", "cupy"])
+@pytest.mark.parametrize("dtype", ["u1", "int64", "float32", "float64"])
+def test_tofile(tmp_path, xp, dtype):
+    """Test tofile()"""
+
+    xp = pytest.importorskip(xp)
+    filepath = str(tmp_path / "test_tofile")
+    src = xp.arange(100, dtype=dtype)
+    tofile(src, filepath)
+
+    dst = xp.fromfile(filepath, dtype=dtype)
+    xp.testing.assert_array_equal(src, dst)
+
+    tofile(src[::2], filepath)
+    dst = xp.fromfile(filepath, dtype=dtype)
+    xp.testing.assert_array_equal(src[::2], dst)
+
+
+@pytest.mark.parametrize("xp", ["numpy", "cupy"])
+@pytest.mark.parametrize("dtype", ["u1", "int64", "float32", "float64"])
+def test_fromfile(tmp_path, xp, dtype):
+    """Test NumPy's and CuPy's fromfile() with LikeWrapper"""
+
+    xp = pytest.importorskip(xp)
+    filepath = str(tmp_path / "test_fromfile")
+    src = xp.arange(100, dtype=dtype)
+    src.tofile(filepath)
+    like = LikeWrapper(like=xp.empty(()))
+    dst = xp.fromfile(filepath, dtype, like=like)
+    xp.testing.assert_array_equal(src, dst)
+    dst = xp.fromfile(filepath, dtype=dtype, like=like)
+    xp.testing.assert_array_equal(src, dst)
+    dst = xp.fromfile(file=filepath, dtype=dtype, like=like)
+    xp.testing.assert_array_equal(src, dst)
+    dst = xp.fromfile(file=filepath, dtype=dtype, count=100 - 42, like=like)
+    xp.testing.assert_array_equal(src[:-42], dst)
+    dst = xp.fromfile(file=filepath, dtype=dtype, offset=src.itemsize, like=like)
+    xp.testing.assert_array_equal(src[1:], dst)
+    dst = xp.fromfile(file=filepath, dtype=dtype, offset=1, count=10, like=like)
+    assert len(dst) == 10
+
+    # Test non-divisible offset
+    dst = xp.fromfile(file=filepath, dtype="u1", offset=7, like=like)
+    xp.testing.assert_array_equal(src.view(dtype="u1")[7:], dst)
+
+    filepath = str(tmp_path / "test_fromfile")
+    with open(filepath, mode="rb") as f:
+        dst = xp.fromfile(file=f, dtype=dtype, like=like)
+        xp.testing.assert_array_equal(src, dst)
+
+
+@pytest.mark.parametrize("xp", ["numpy", "cupy"])
+def test_fromfile_error(tmp_path, xp):
+    xp = pytest.importorskip(xp)
+    filepath = str(tmp_path / "test_fromfile")
+    src = xp.arange(1, dtype="u1")
+    src.tofile(filepath)
+    like = LikeWrapper(like=src)
+
+    with pytest.raises(FileNotFoundError, match="no file"):
+        xp.fromfile("no file", like=like)
+
+    with pytest.raises(NotImplementedError, match="Non-default value of the `sep`"):
+        xp.fromfile(file=filepath, sep=",", like=like)
+
+    with pytest.raises(ValueError, match="[Nn]egative dimensions are not allowed"):
+        xp.fromfile(file=filepath, like=like, count=-42)
