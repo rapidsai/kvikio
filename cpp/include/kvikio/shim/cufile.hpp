b0c6cedf43d1bc240c3ef1b38ebb9d89574a08ee
Mads R. B. Kristensen <madsbk@gmail.com>
2022-10-28 09:04:35 +0200

    dlopen: now trying "libcufile.so.1", "libcufile.so.0", "libcufile.so" (#141)
    Fixes #140
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    Approvers:
      - Benjamin Zaitlen (https://github.com/quasiben)
    
    URL: https://github.com/rapidsai/kvikio/pull/141

--- cpp/include/kvikio/shim/cufile.hpp
+++ cpp/include/kvikio/shim/cufile.hpp
@@ -49,7 +49,7 @@ class cuFileAPI {
  private:
   cuFileAPI()
   {
-    void* lib = load_library("libcufile.so.1");
+    void* lib = load_library({"libcufile.so.1", "libcufile.so.0", "libcufile.so"});
     get_symbol(HandleRegister, lib, KVIKIO_STRINGIFY(cuFileHandleRegister));
     get_symbol(HandleDeregister, lib, KVIKIO_STRINGIFY(cuFileHandleDeregister));
     get_symbol(Read, lib, KVIKIO_STRINGIFY(cuFileRead));
