d343eb1c7812af8cdfe328bc70cedde0d7c8b109
Ajay Thorve <ajaythorve1994@gmail.com>
2023-01-27 17:19:52 -0800

    add cpp and python build scripts

---
+++ ci/build_cpp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source rapids-env-update
+
+export CMAKE_GENERATOR=Ninja
+
+rapids-print-env
+
+rapids-logger "Begin cpp build"
+
+rapids-mamba-retry mambabuild conda/recipes/libkvikio
+
+rapids-upload-conda-to-s3 cpp
