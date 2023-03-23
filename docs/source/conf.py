71feb70880e50b76406289083ea83e95cb459972
Raymond Douglass <ray@raydouglass.com>
2023-03-23 14:58:47 -0400

    DOC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2022, NVIDIA"
 author = "NVIDIA"
 
 # The short X.Y version.
-version = '23.04'
+version = '23.06'
 # The full version, including alpha/beta/rc tags
-release = '23.04.00'
+release = '23.06.00'
 
 
 # -- General configuration ---------------------------------------------------
