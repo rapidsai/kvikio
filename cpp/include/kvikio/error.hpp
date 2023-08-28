5358b3529873eb1d54bf40b8991e50a6d2c50a56
Mads R. B. Kristensen <madsbk@gmail.com>
2023-08-28 16:10:52 +0200

    dummy CUFILE_CHECK_STREAM_IO

--- cpp/include/kvikio/error.hpp
+++ cpp/include/kvikio/error.hpp
@@ -79,13 +79,13 @@ struct CUfileException : public std::runtime_error {
 #endif
 #endif
 
-#ifdef KVIKIO_CUFILE_FOUND
 #ifndef CUFILE_CHECK_STREAM_IO
 #define CUFILE_CHECK_STREAM_IO(...)                                  \
   GET_CUFILE_CHECK_STREAM_IO_MACRO(                                  \
     __VA_ARGS__, CUFILE_CHECK_STREAM_IO_2, CUFILE_CHECK_STREAM_IO_1) \
   (__VA_ARGS__)
 #define GET_CUFILE_CHECK_STREAM_IO_MACRO(_1, _2, NAME, ...) NAME
+#ifdef KVIKIO_CUFILE_FOUND
 #define CUFILE_CHECK_STREAM_IO_2(_nbytes_done, _exception_type)                                    \
   do {                                                                                             \
     int const _nbytes = (_nbytes_done);                                                            \
@@ -95,8 +95,13 @@ struct CUfileException : public std::runtime_error {
                              cufileop_status_error((CUfileOpError)(CUFILEOP_BASE_ERR - _nbytes))}; \
     }                                                                                              \
   } while (0)
-#define CUFILE_CHECK_STREAM_IO_1(_call) CUFILE_CHECK_STREAM_IO_2(_call, kvikio::CUfileException)
+#else
+// if cufile isn't available, we don't do anything in the body
+#define CUFILE_CHECK_STREAM_IO_2(_nbytes_done, _exception_type) \
+  do {                                                          \
+  } while (0)
 #endif
+#define CUFILE_CHECK_STREAM_IO_1(_call) CUFILE_CHECK_STREAM_IO_2(_call, kvikio::CUfileException)
 #endif
 
 }  // namespace kvikio
