2dec32ab4ee754dea8c457636a5dd7ba63cae75b
ptaylor <paul.e.taylor@me.com>
2024-02-07 06:26:59 +0000

    check if libkvikio gtests dir exists before cd'ing

--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -3,8 +3,9 @@
 
 set -euo pipefail
 
-# Support customizing the ctests' install location
-cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/tests/libkvikio/"
-
-# Run BASIC_IO_TEST
-./BASIC_IO_TEST
+if [ -d "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/tests/libkvikio/" ]; then
+    # Support customizing the ctests' install location
+    cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/tests/libkvikio/"
+    # Run BASIC_IO_TEST
+    ./BASIC_IO_TEST
+fi
