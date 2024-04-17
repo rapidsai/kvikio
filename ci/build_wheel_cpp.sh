1faa5173d845aa15df13776175a7db359c8fdaae
Michael Sarahan <msarahan@nvidia.com>
2024-04-17 10:26:04 -0700

    reorganize python folder to allow for libkvikio wheel

--- ci/build_wheel_cpp.sh
+++ ci/build_wheel_cpp.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 package_name="kvikio"
-package_dir="python/kvikio"
+package_dir="python/libkvikio"
 
 source rapids-configure-sccache
 source rapids-date-string
@@ -28,13 +28,11 @@ if [[ $PACKAGE_CUDA_SUFFIX == "-cu12" ]]; then
     sed -i "s/cuda-python[<=>\.,0-9a]*/cuda-python>=12.0,<13.0a0/g" ${pyproject_file}
 fi
 
-CPP_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/libkvikio_dist)
-
 cd "${package_dir}"
 
-python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check --find-links ${CPP_WHEELHOUSE}
+python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
 
 mkdir -p final_dist
 python -m auditwheel repair -w final_dist dist/*
 
-RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
+RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp final_dist
