f4fbf11fd9bf7e38f28b639038ed8ee72863610f
Michael Sarahan <msarahan@nvidia.com>
2024-05-07 14:01:11 -0500

    add simple wheel install/test script

---
+++ ci/test_wheel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+
+set -eou pipefail
+
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+WHEELHOUSE="${PWD}/dist/"
+RAPIDS_PY_WHEEL_NAME="kvikio_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python "${WHEELHOUSE}"
+
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install "kvikio-${RAPIDS_PY_CUDA_SUFFIX}[test]>=0.0.0a0" --find-links "${WHEELHOUSE}"
+
+python -m pytest ./python/kvikio/kvikio/tests
