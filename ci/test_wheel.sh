8f058b9f6c8b5ae1ecfd36d045a99ce8e7a883f7
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2024-06-07 14:10:53 -0700

    Apply suggestions from code review
    Co-authored-by: James Lamb <jaylamb20@gmail.com>

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -7,6 +7,6 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 WHEELHOUSE="${PWD}/dist/"
 RAPIDS_PY_WHEEL_NAME="kvikio_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python "${WHEELHOUSE}"
 
-python -m pip install "kvikio-${RAPIDS_PY_CUDA_SUFFIX}[test]>=0.0.0a0" --find-links "${WHEELHOUSE}"
+python -m pip install "$(echo ${WHEELHOUSE}/kvikio_${RAPIDS_PY_CUDA_SUFFIX}*.whl)[test]"
 
 python -m pytest ./python/kvikio/tests
