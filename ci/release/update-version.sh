89fd81a6f85f74dfdd4ff885ebdd0e50f6fa5c9c
Paul Taylor <178183+trxcllnt@users.noreply.github.com>
2024-07-24 23:37:52 -0700

    add --rm and --name to devcontainer run args (#370)
    * Update the `cuda11.8-conda` devcontainer's base image
    * Remove the devcontainer when the VSCode window closes
    * Adds a descriptive name to the running container:
      ```shell
      $ docker ps -a
      CONTAINER ID   IMAGE           ...  NAMES
      0dbb364fe544   vsc-kvikio-...  ...  rapids-kvikio-24.06-cuda12.2-conda
      
      $ docker rm -f rapids-kvikio-24.06-cuda12.2-conda
      ```
    
    Authors:
      - Paul Taylor (https://github.com/trxcllnt)
    
    Approvers:
      - Ray Douglass (https://github.com/raydouglass)
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - https://github.com/jakirkham
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/kvikio/pull/370

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -57,6 +57,7 @@ done
 find .devcontainer/ -type f -name devcontainer.json -print0 | while IFS= read -r -d '' filename; do
     sed_runner "s@rapidsai/devcontainers:[0-9.]*@rapidsai/devcontainers:${NEXT_SHORT_TAG}@g" "${filename}"
     sed_runner "s@rapidsai/devcontainers/features/rapids-build-utils:[0-9.]*@rapidsai/devcontainers/features/rapids-build-utils:${NEXT_SHORT_TAG_PEP440}@" "${filename}"
+    sed_runner "s@rapids-\${localWorkspaceFolderBasename}-[0-9.]*@rapids-\${localWorkspaceFolderBasename}-${NEXT_SHORT_TAG}@g" "${filename}"
 done
 
 # The example of a downstream project
