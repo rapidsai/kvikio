0cae56bb1de93c1f26534679e9c2bc233438f9cc
Mads R. B. Kristensen <madsbk@gmail.com>
2024-08-09 16:04:08 +0200

    remove kvikio_cxx_api.pxd

--- python/kvikio/kvikio/_lib/libkvikio.pyx
+++ python/kvikio/kvikio/_lib/libkvikio.pyx
@@ -7,19 +7,85 @@
 import pathlib
 from typing import Optional
 
+from posix cimport fcntl
+
 from libc.stdint cimport uintptr_t
+from libcpp cimport bool
+from libcpp.string cimport string
 from libcpp.utility cimport move, pair
 
+from kvikio._lib.arr cimport Array
 from kvikio._lib.future cimport (
     IOFuture,
     IOFutureStream,
     _wrap_io_future,
     _wrap_stream_future,
+    cpp_StreamFuture,
+    future,
 )
-from kvikio._lib import defaults
-from kvikio._lib.arr cimport Array
 
-from .kvikio_cxx_api cimport CUstream, FileHandle
+from kvikio._lib import defaults
+
+
+cdef extern from "cuda.h":
+    ctypedef void* CUstream
+
+
+cdef extern from "<kvikio/file_handle.hpp>" namespace "kvikio" nogil:
+    cdef cppclass FileHandle:
+        FileHandle() except +
+        FileHandle(int fd) except +
+        FileHandle(
+            string file_path,
+            string flags,
+        ) except +
+        FileHandle(
+            string file_path,
+            string flags,
+            fcntl.mode_t mode
+        ) except +
+        void close()
+        bool closed()
+        int fd()
+        int fd_open_flags() except +
+        future[size_t] pread(
+            void* devPtr,
+            size_t size,
+            size_t file_offset,
+            size_t task_size
+        ) except +
+        future[size_t] pwrite(
+            void* devPtr,
+            size_t size,
+            size_t file_offset,
+            size_t task_size
+        ) except +
+        size_t read(
+            void* devPtr_base,
+            size_t size,
+            size_t file_offset,
+            size_t devPtr_offset
+        ) except +
+        size_t write(
+            void* devPtr_base,
+            size_t size,
+            size_t file_offset,
+            size_t devPtr_offset
+        ) except +
+        cpp_StreamFuture read_async(
+            void* devPtr_base,
+            size_t size,
+            size_t file_offset,
+            size_t devPtr_offset,
+            CUstream stream
+        ) except +
+        cpp_StreamFuture write_async(
+            void* devPtr_base,
+            size_t size,
+            size_t file_offset,
+            size_t devPtr_offset,
+            CUstream stream
+        ) except +
 
 
 cdef pair[uintptr_t, size_t] _parse_buffer(buf, size, bint accept_host_buffer) except *:
