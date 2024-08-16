c0417290a13889b33a718d5c6ca316716e71632e
Mads R. B. Kristensen <madsbk@gmail.com>
2024-08-16 10:06:04 +0200

    DevBuffer

--- cpp/tests/utils.hpp
+++ cpp/tests/utils.hpp
@@ -19,16 +19,59 @@
 #include <iostream>
 #include <string>
 
+#include <cuda_runtime_api.h>
 #include <gtest/gtest.h>
 
+#include <kvikio/error.hpp>
+
 namespace kvikio::test {
+
+/**                                                                                  \
+ * @brief Error checking macro for CUDA runtime API functions.                       \
+ *                                                                                   \
+ * Invokes a CUDA runtime API function call. If the call does not return             \
+ * `cudaSuccess`, invokes cudaGetLastError() to clear the error and throws an        \
+ * exception detailing the CUDA error that occurred                                  \
+ *                                                                                   \
+ * Defaults to throwing std::runtime_error, but a custom exception may also be       \
+ * specified.                                                                        \
+ *                                                                                   \
+ * Example:                                                                          \
+ * ```c++                                                                            \
+ *                                                                                   \
+ * // Throws rmm::cuda_error if `cudaMalloc` fails                                   \
+ * KVIKIO_CHECK_CUDA(cudaMalloc(&p, 100));                                           \
+ *                                                                                   \
+ * // Throws std::runtime_error if `cudaMalloc` fails                                \
+ * KVIKIO_CHECK_CUDA(cudaMalloc(&p, 100), std::runtime_error);                       \
+ * ```                                                                               \
+ *                                                                                   \
+ */                                                                                  \
+#define KVIKIO_CHECK_CUDA(...)                                                       \
+  GET_KVIKIO_CHECK_CUDA_MACRO(__VA_ARGS__, KVIKIO_CHECK_CUDA_2, KVIKIO_CHECK_CUDA_1) \
+  (__VA_ARGS__)
+#define GET_KVIKIO_CHECK_CUDA_MACRO(_1, _2, NAME, ...) NAME
+#define KVIKIO_CHECK_CUDA_2(_call, _exception_type)                                             \
+  do {                                                                                          \
+    cudaError_t const error = (_call);                                                          \
+    if (cudaSuccess != error) {                                                                 \
+      cudaGetLastError();                                                                       \
+      /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/                                            \
+      throw _exception_type{std::string{"CUDA error at: "} + __FILE__ + ":" +                   \
+                            KVIKIO_STRINGIFY(__LINE__) + ": " + cudaGetErrorName(error) + " " + \
+                            cudaGetErrorString(error)};                                         \
+    }                                                                                           \
+  } while (0)
+#define KVIKIO_CHECK_CUDA_1(_call) KVIKIO_CHECK_CUDA_2(_call, std::runtime_error)
 
 class TempDir {
  public:
   TempDir(const bool cleanup = true) : _cleanup{cleanup}
   {
     std::string tpl{std::filesystem::temp_directory_path() / "legate-dataframe.XXXXXX"};
-    if (mkdtemp(tpl.data()) == nullptr) {}
+    if (mkdtemp(tpl.data()) == nullptr) {
+      throw std::runtime_error("TempDir: cannot make tmpdir: " + tpl);
+    }
     _dir_path = tpl;
   }
   ~TempDir() noexcept
@@ -42,13 +85,31 @@ class TempDir {
     }
   }
 
+  TempDir(const TempDir&)              = delete;
+  TempDir& operator=(TempDir const&)   = delete;
+  TempDir(const TempDir&&)             = delete;
+  TempDir&& operator=(TempDir const&&) = delete;
+
   const std::filesystem::path& path() { return _dir_path; }
 
   operator std::string() { return path(); }
 
  private:
   const bool _cleanup;
-  std::filesystem::path _dir_path;
+  std::filesystem::path _dir_path{};
+};
+
+class DevBuffer {
+ public:
+  DevBuffer(std::size_t nbytes = 1024) : nbytes{nbytes}
+  {
+    KVIKIO_CHECK_CUDA(cudaMalloc(&ptr, nbytes));
+  }
+
+  ~DevBuffer() noexcept { cudaFree(ptr); }
+
+  const std::size_t nbytes;
+  void* ptr{nullptr};
 };
 
 }  // namespace kvikio::test
