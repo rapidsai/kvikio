500681a35b451d3c947b38ed94450abc568f31ef
Kyle Edwards <kyedwards@nvidia.com>
2024-08-28 16:06:44 -0400

    Style

--- cpp/cmake/thirdparty/get_aws_sdk_cpp.cmake
+++ cpp/cmake/thirdparty/get_aws_sdk_cpp.cmake
@@ -18,8 +18,7 @@ function(find_and_configure_aws_sdk_cpp)
 
   rapids_cpm_find(
     AWSSDK 1.11.393
-    GLOBAL_TARGETS aws-cpp-sdk-s3
-    COMPONENTS S3
+    GLOBAL_TARGETS aws-cpp-sdk-s3 COMPONENTS S3
     BUILD_EXPORT_SET kvikio-exports
     INSTALL_EXPORT_SET kvikio-exports
     CPM_ARGS
