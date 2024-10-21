e8ecb4879962e35bd7038824a2ad866bb4f749fb
Robert Maynard <rmaynard@nvidia.com>
2024-10-21 16:41:54 -0400

    Correct style issues found by CI

--- cpp/cmake/thirdparty/get_libcurl.cmake
+++ cpp/cmake/thirdparty/get_libcurl.cmake
@@ -35,7 +35,8 @@ function(find_and_configure_libcurl)
   if(DEFINED CACHE_HAS_BUILD_TESTING)
     set(BUILD_TESTING
         ${CACHE_HAS_BUILD_TESTING}
-        CACHE BOOL "" FORCE)
+        CACHE BOOL "" FORCE
+    )
   else()
     unset(BUILD_TESTING CACHE)
   endif()
