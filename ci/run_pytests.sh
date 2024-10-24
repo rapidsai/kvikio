f8c83ab598a9fdd0089d9246ea3e5a9a20fb2a2d
Mads R. B. Kristensen <madsbk@gmail.com>
2024-10-24 18:51:32 +0200

    CI: skip tests marked "cufile" on cuda 11.8 and arm64

--- ci/run_pytests.sh
+++ ci/run_pytests.sh
@@ -6,4 +6,11 @@ set -euo pipefail
 # Support invoking run_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/kvikio
 
-pytest --cache-clear --verbose "$@" tests
+# If running CUDA 11.8 on arm64, we skip tests marked "cufile".
+# cuFile didn't support arm until 12.4
+PYTEST_MARK=$( \
+  [["${CUDA_VERSION}" == "11.8.0" && "${NVARCH}" == "sbsa" ]] \
+  && echo "-m 'not cufile'" || echo "" \
+)
+
+python -m pytest ${PYTEST_MARK} --cache-clear --verbose "$@" tests
