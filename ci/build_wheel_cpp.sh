10bc842eb9abb7af272f7d31fbeef310743ad062
James Lamb <jlamb@nvidia.com>
2024-10-29 14:25:37 -0500

    print sccache stats in builds (#524)
    Contributes to https://github.com/rapidsai/build-planning/issues/111
    
    Proposes some small packaging/CI changes, matching similar changes being made across RAPIDS.
    
    * printing `sccache` stats to CI logs
    * updating to the latest `rapids-dependency-file-generator` (v1.16.0)
    * always explicitly specifying `cpp` / `python` in calls to `rapids-upload-wheels-to-s3`
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
    
    URL: https://github.com/rapidsai/kvikio/pull/524

--- ci/build_wheel_cpp.sh
+++ ci/build_wheel_cpp.sh
@@ -13,8 +13,12 @@ rapids-generate-version > ./VERSION
 
 cd "${package_dir}"
 
+sccache --zero-stats
+
 python -m pip install wheel
-python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+python -m pip wheel . -w dist -v --no-deps --disable-pip-version-check
+
+sccache --show-adv-stats
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp dist
