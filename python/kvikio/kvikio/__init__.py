1c9984189828d501e74e1d9a9cca199c9377bcd2
Mads R. B. Kristensen <madsbk@gmail.com>
2024-11-06 08:00:26 +0100

    Build KvikIO as a shared library (#527)
    Moving libcurl dependent functions to `remote_handle.cpp` (let's move the rest in a follow up PR) and setup building `libkvikio.so`.
    
    ### Background 
    
    Now that KvikIO has evolved into a standalone IO library that works without cuFile and CUDA, I think it makes sense to move to a shared library. Originally, KvikIO was a very thin wrapper around cuFile, which is why we decided to keep it header-only (not because of templates). 
    
    **Pros**:
      - Enables us to statically compile libcurl into `libkvikio.so`. 
        * Required by our wheels. If we don’t do this, we will have to find another solution to https://github.com/rapidsai/kvikio/issues/512 
      - Avoid having to compile libcurl in downstream projects. Currently, cudf must compile libcurl in every build. 
      - Reduce compile time in CI, both for KvikIO and downstream projects. 
      - Ease development by not having to rebuild downstream projects like cudf every time KvikIO is modified. 
    
    **Cons**: 
     - Projects cannot vendoring KvikIO as a header-only project. As far as we know, nobody does this.
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - Paul Taylor (https://github.com/trxcllnt)
    
    Approvers:
      - Robert Maynard (https://github.com/robertmaynard)
      - Vyas Ramasubramani (https://github.com/vyasr)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/kvikio/pull/527

--- python/kvikio/kvikio/__init__.py
+++ python/kvikio/kvikio/__init__.py
@@ -1,6 +1,17 @@
 # Copyright (c) 2021-2024, NVIDIA CORPORATION. All rights reserved.
 # See file LICENSE for terms.
 
+# If libkvikio was installed as a wheel, we must request it to load the library symbols.
+# Otherwise, we assume that the library was installed in a system path that ld can find.
+try:
+    import libkvikio
+except ModuleNotFoundError:
+    pass
+else:
+    libkvikio.load_library()
+    del libkvikio
+
+
 from kvikio._version import __git_commit__, __version__
 from kvikio.cufile import CuFile
 from kvikio.remote_file import RemoteFile, is_remote_file_available
