a775bc02554a763e3bf5c8754ff46f6492bbaf67
Robert Maynard <rmaynard@nvidia.com>
2024-11-08 10:26:21 -0500

    Improve debug output

--- conda/recipes/libkvikio/build.sh
+++ conda/recipes/libkvikio/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Copyright (c) 2023-2024, NVIDIA CORPORATION.
 
+touch empty.cu
 nvcc -c empty.cu -v
+rm empty.cu
 
-CUDACXX=nvcc ./build.sh -v -n libkvikio
+CUDACXX=nvcc ./build.sh -v -n libkvikio --cmake-args=\"--trace-expand\"
