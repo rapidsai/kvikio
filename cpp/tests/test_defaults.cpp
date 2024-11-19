efae9b039854814f8ae936bcb3ca0ac2b32493ef
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-11-19 14:40:46 -0500

    Cleanup

--- cpp/tests/test_defaults.cpp
+++ cpp/tests/test_defaults.cpp
@@ -38,7 +38,7 @@ TEST(Defaults, parse_compat_mode_str)
   }
 
   {
-    std::vector<std::string> inputs{"AUTO", "auto", "aUtO", "WHATEVER", "whatever", "WhatEver"};
+    std::vector<std::string> inputs{"AUTO", "auto", "aUtO"};
     for (const auto& input : inputs) {
       EXPECT_EQ(kvikio::detail::parse_compat_mode_str(input), kvikio::CompatMode::AUTO);
     }
