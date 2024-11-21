c3064b7e4e4912120b7633472931b484bf4f8f54
Tianyu Liu <tialiu@nvidia.com>
2024-11-21 00:05:49 +0000

    Fix Python formatting

--- python/kvikio/tests/test_async_io.py
+++ python/kvikio/tests/test_async_io.py
@@ -32,7 +32,9 @@ def test_read_write(tmp_path, size):
     # Try to read file opened in write-only mode
     # POSIX read would yield the error "Operation not permitted"
     # cuFile read would yield the error "unsupported file open flags"
-    with pytest.raises(RuntimeError, match="Operation not permitted|unsupported file open flags"):
+    with pytest.raises(
+        RuntimeError, match="Operation not permitted|unsupported file open flags"
+    ):
         # The exception is raised when we call the raw_read_async API.
         future_stream = f.raw_read_async(a, stream.ptr)
         future_stream.check_bytes_done()
