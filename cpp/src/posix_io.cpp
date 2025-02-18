096ac0ffa73db4b09b8c2e2089c4c83713fe1622
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-02-18 03:57:45 -0500

    Enable tracing of thread pool tasks using NVTX (#630)
    This PR implements the basic feature outlined in #631. 
    The two good-to-haves are currently blocked.
    
    Authors:
      - Tianyu Liu (https://github.com/kingcrimsontianyu)
    
    Approvers:
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - Lawrence Mitchell (https://github.com/wence-)
    
    URL: https://github.com/rapidsai/kvikio/pull/630

--- cpp/src/posix_io.cpp
+++ cpp/src/posix_io.cpp
@@ -22,6 +22,7 @@
 
 #include <kvikio/bounce_buffer.hpp>
 #include <kvikio/error.hpp>
+#include <kvikio/nvtx.hpp>
 #include <kvikio/posix_io.hpp>
 #include <kvikio/shim/cuda.hpp>
 #include <kvikio/utils.hpp>
