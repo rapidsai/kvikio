ae2ea11b24760259011492e2aa3b119e81b3bba8
Tom Augspurger <toaugspurger@nvidia.com>
2025-02-21 22:50:06 +0000

    Added include to new module

--- cpp/include/kvikio/http_status_codes.hpp
+++ cpp/include/kvikio/http_status_codes.hpp
@@ -18,6 +18,7 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 namespace kvikio {
 namespace detail {
--- cpp/src/http_status_codes.cpp
+++ cpp/src/http_status_codes.cpp
@@ -20,6 +20,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace kvikio {
 
