99c744bcbdc80d148528c65d7ef971b646b41ec1
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-03-12 10:12:22 -0400

    Finish threadpool benchmark

--- cpp/benchmarks/threadpool/threadpool_benchmark.cpp
+++ cpp/benchmarks/threadpool/threadpool_benchmark.cpp
@@ -14,14 +14,43 @@
  * limitations under the License.
  */
 
+#include <cmath>
+
 #include <benchmark/benchmark.h>
 #include <kvikio/defaults.hpp>
 
-void BM_threadpool(benchmark::State& state)
+void compute()
 {
-  for (auto _ : state) {
-    std::string empty_string;
+  std::size_t const num_compute_iterations{100'000u};
+  [[maybe_unused]] double res{0.0};
+  for (std::size_t i = 0u; i < num_compute_iterations; ++i) {
+    auto x{static_cast<double>(i)};
+    res += std::sqrt(x) + std::cbrt(x) + std::sin(x);
   }
 }
 
-BENCHMARK(BM_threadpool)->Name("KvikIO_threadpool");
+void BM_threadpool_compute(benchmark::State& state)
+{
+  std::size_t const num_compute_tasks{10'000u};
+  for (auto _ : state) {
+    state.PauseTiming();
+    kvikio::defaults::set_thread_pool_nthreads(state.range(0));
+
+    state.ResumeTiming();
+    for (std::size_t i = 0u; i < num_compute_tasks; ++i) {
+      [[maybe_unused]] auto fut = kvikio::defaults::thread_pool().submit_task(compute);
+    }
+    kvikio::defaults::thread_pool().wait();
+  }
+}
+
+int main(int argc, char** argv)
+{
+  benchmark::Initialize(&argc, argv);
+  benchmark::RegisterBenchmark("BM_threadpool_compute", BM_threadpool_compute)
+    ->RangeMultiplier(2)
+    ->Range(1, 64)
+    ->Unit(benchmark::kMillisecond);
+  benchmark::RunSpecifiedBenchmarks();
+  benchmark::Shutdown();
+}
