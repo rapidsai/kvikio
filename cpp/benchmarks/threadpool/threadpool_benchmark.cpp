6395f210a27429a26d3376c30b762e51216d1643
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-03-14 00:46:43 -0400

    Address comments and fix issues

--- cpp/benchmarks/threadpool/threadpool_benchmark.cpp
+++ cpp/benchmarks/threadpool/threadpool_benchmark.cpp
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 
-// This benchmark assesses the performance of the thread pool.
+// This benchmark assesses the scalability of the thread pool.
+//
 // In the "strong scaling" study, the total amount of tasks is fixed, and the time to complete
 // these tasks is evaluated as a function of thread count.
-// In the "weak scaling" study, the expected tasks per thread is fixed, and the total amount of
-// tasks is then proportional to the thread count. Again, the time is evaluated as a function of
-// thread count.
+//
+// In the "weak scaling" study, the expected amount of tasks per thread is fixed, and the total
+// amount of tasks is then proportional to the thread count. Again, the time is evaluated as a
+// function of thread count.
 
 #include <cmath>
 #include <cstdint>
@@ -29,8 +31,8 @@
 
 namespace kvikio {
 enum class ScalingType : uint8_t {
-  StrongScaling,
-  WeakScaling,
+  STRONG_SCALING,
+  WEAK_SCALING,
 };
 
 void task_compute(std::size_t num_compute_iterations)
@@ -38,28 +40,24 @@ void task_compute(std::size_t num_compute_iterations)
   [[maybe_unused]] double res{0.0};
   for (std::size_t i = 0u; i < num_compute_iterations; ++i) {
     auto x{static_cast<double>(i)};
-    res += std::sqrt(x) + std::cbrt(x) + std::sin(x);
+    benchmark::DoNotOptimize(res += std::sqrt(x) + std::cbrt(x) + std::sin(x));
   }
 }
 
 template <ScalingType scaling_type>
 void BM_threadpool_compute(benchmark::State& state)
 {
-  auto num_threads = state.range(0);
+  auto const num_threads = state.range(0);
 
-  std::string label;
-  std::size_t num_compute_tasks;
-  if constexpr (scaling_type == ScalingType::StrongScaling) {
-    num_compute_tasks = 10'000;
-  } else {
-    num_compute_tasks = 1'000 * num_threads;
-  }
+  std::size_t const num_compute_tasks =
+    (scaling_type == ScalingType::STRONG_SCALING) ? 10'000 : (1'000 * num_threads);
 
-  std::size_t const num_compute_iterations{100'000};
+  std::size_t const num_compute_iterations{1'000};
   kvikio::defaults::set_thread_pool_nthreads(num_threads);
 
   for (auto _ : state) {
-    for (std::size_t i = 0u; i < num_compute_tasks; ++i) {
+    // Submit a total of "num_compute_tasks" tasks to the thread pool.
+    for (auto i = std::size_t{0}; i < num_compute_tasks; ++i) {
       [[maybe_unused]] auto fut = kvikio::defaults::thread_pool().submit_task(
         [num_compute_iterations = num_compute_iterations] {
           task_compute(num_compute_iterations);
@@ -77,16 +75,20 @@ int main(int argc, char** argv)
   benchmark::Initialize(&argc, argv);
 
   benchmark::RegisterBenchmark("BM_threadpool_compute:strong_scaling",
-                               kvikio::BM_threadpool_compute<kvikio::ScalingType::StrongScaling>)
+                               kvikio::BM_threadpool_compute<kvikio::ScalingType::STRONG_SCALING>)
     ->RangeMultiplier(2)
-    ->Range(1, 64)
-    ->Unit(benchmark::kMillisecond);
+    ->Range(1, 64)   // Increase from 1 to 64 (inclusive of both endpoints) with x2 stepping.
+    ->UseRealTime()  // Use the wall clock to determine the number of benchmark iterations.
+    ->Unit(benchmark::kMillisecond)
+    ->MinTime(2);  // Minimum of 2 seconds.
 
   benchmark::RegisterBenchmark("BM_threadpool_compute:weak_scaling",
-                               kvikio::BM_threadpool_compute<kvikio::ScalingType::WeakScaling>)
+                               kvikio::BM_threadpool_compute<kvikio::ScalingType::WEAK_SCALING>)
     ->RangeMultiplier(2)
     ->Range(1, 64)
-    ->Unit(benchmark::kMillisecond);
+    ->UseRealTime()
+    ->Unit(benchmark::kMillisecond)
+    ->MinTime(2);
 
   benchmark::RunSpecifiedBenchmarks();
   benchmark::Shutdown();
