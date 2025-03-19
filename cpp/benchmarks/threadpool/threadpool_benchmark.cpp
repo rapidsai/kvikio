489d9a9a4d8459bd7af0ac71c205fb0a4d247ae6
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-03-19 00:15:52 -0400

    Improve clarity

--- cpp/benchmarks/threadpool/threadpool_benchmark.cpp
+++ cpp/benchmarks/threadpool/threadpool_benchmark.cpp
@@ -37,6 +37,12 @@ enum class ScalingType : uint8_t {
   WEAK_SCALING,
 };
 
+namespace constant {
+std::size_t constexpr ntasks_strong_scaling{10'000};
+std::size_t constexpr ntasks_weak_scaling{1'000};
+std::size_t constexpr num_compute_iterations{1'000};
+}  // namespace constant
+
 void task_compute(std::size_t num_compute_iterations)
 {
   [[maybe_unused]] double res{0.0};
@@ -51,17 +57,17 @@ void BM_BS_threadpool_compute(benchmark::State& state)
 {
   auto const num_threads = state.range(0);
 
-  std::size_t const num_compute_tasks =
-    (scaling_type == ScalingType::STRONG_SCALING) ? 10'000 : (1'000 * num_threads);
+  std::size_t const num_compute_tasks = (scaling_type == ScalingType::STRONG_SCALING)
+                                          ? constant::ntasks_strong_scaling
+                                          : (constant::ntasks_weak_scaling * num_threads);
 
-  std::size_t constexpr num_compute_iterations{1'000};
   kvikio::defaults::set_thread_pool_nthreads(num_threads);
 
   for (auto _ : state) {
     // Submit a total of "num_compute_tasks" tasks to the thread pool.
     for (auto i = std::size_t{0}; i < num_compute_tasks; ++i) {
-      [[maybe_unused]] auto fut =
-        kvikio::defaults::thread_pool().submit_task([] { task_compute(num_compute_iterations); });
+      [[maybe_unused]] auto fut = kvikio::defaults::thread_pool().submit_task(
+        [] { task_compute(constant::num_compute_iterations); });
     }
     kvikio::defaults::thread_pool().wait();
   }
@@ -74,17 +80,17 @@ void BM_simple_threadpool_compute(benchmark::State& state)
 {
   auto const num_threads = state.range(0);
 
-  std::size_t const num_compute_tasks =
-    (scaling_type == ScalingType::STRONG_SCALING) ? 10'000 : (1'000 * num_threads);
+  std::size_t const num_compute_tasks = (scaling_type == ScalingType::STRONG_SCALING)
+                                          ? constant::ntasks_strong_scaling
+                                          : (constant::ntasks_weak_scaling * num_threads);
 
-  std::size_t constexpr num_compute_iterations{1'000};
   kvikio::ThreadPoolSimple thread_pool(num_threads);
 
   for (auto _ : state) {
     // Submit a total of "num_compute_tasks" tasks to the thread pool.
     for (auto i = std::size_t{0}; i < num_compute_tasks; ++i) {
       [[maybe_unused]] auto fut =
-        thread_pool.submit_task([] { task_compute(num_compute_iterations); });
+        thread_pool.submit_task([] { task_compute(constant::num_compute_iterations); });
     }
     thread_pool.wait();
   }
@@ -118,10 +124,10 @@ int main(int argc, char** argv)
     "simple_threadpool_compute:strong_scaling",
     kvikio::BM_simple_threadpool_compute<kvikio::ScalingType::STRONG_SCALING>)
     ->RangeMultiplier(2)
-    ->Range(1, 64)   // Increase from 1 to 64 (inclusive of both endpoints) with x2 stepping.
-    ->UseRealTime()  // Use the wall clock to determine the number of benchmark iterations.
+    ->Range(1, 64)
+    ->UseRealTime()
     ->Unit(benchmark::kMillisecond)
-    ->MinTime(2);  // Minimum of 2 seconds.
+    ->MinTime(2);
 
   benchmark::RegisterBenchmark(
     "simple_threadpool_compute:weak_scaling",
