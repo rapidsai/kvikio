6e3115bd2bc85b5708c6fbcba2b3caa8a8c77279
Gil Forsyth <gforsyth@nvidia.com>
2025-03-21 15:50:36 -0400

    don't set strict for 11.8 python builds

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -4,11 +4,17 @@
 set -euo pipefail
 
 rapids-configure-conda-channels
-# Setting channel priority per-repo until all RAPIDS can build using strict channel priority
-# This will be replaced when we port this recipe to `rattler-build`
-conda config --set channel_priority strict
 # `rapids-configure-conda-channels` should only insert `rapidsai` channel into release builds
 conda config --remove channels rapidsai
+
+# Setting channel priority per-repo until all RAPIDS can build using strict channel priority
+# This will be replaced when we port this recipe to `rattler-build`
+if [[ "$RAPIDS_CUDA_VERSION" == 11.8* && "$RAPIDS_CONDA_ARCH" == "linux64" ]]; then
+  rapids-logger "Not setting strict priority because of libcufile version constraints"
+else
+  rapids-logger "Setting strict channel priority for conda builds"
+  conda config --set channel_priority strict
+fi
 
 source rapids-configure-sccache
 
