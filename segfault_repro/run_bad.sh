4ad05f6245db723f509a95eb4b447c59abf05c26
Tianyu Liu <tialiu@nvidia.com>
2025-04-04 03:58:20 +0000

    Update

--- segfault_repro/run_bad.sh
+++ segfault_repro/run_bad.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rm -rf biu_check_good_tmp
-mkdir biu_check_good_tmp
+rm -rf biu_check_bad_tmp
+mkdir biu_check_bad_tmp
 
-g++ -isystem /usr/local/cuda/targets/sbsa-linux/include -g -o biu_check_good_tmp/biu.cpp.o -c biu.cpp
+g++ -isystem /usr/local/cuda/targets/sbsa-linux/include -g -o biu_check_bad_tmp/biu.cpp.o -c biu.cpp
 
-g++ -g biu_check_good_tmp/biu.cpp.o -o biu_check_good \
+g++ -g biu_check_bad_tmp/biu.cpp.o -o biu_check_bad \
 -Wl,-rpath,/usr/local/cuda-12.8/targets/sbsa-linux/lib \
 -ldl
 
@@ -13,6 +13,6 @@ export CUFILE_ALLOW_COMPAT_MODE=false
 export CUFILE_FORCE_COMPAT_MODE=false
 export CUFILE_ENV_PATH_JSON=my_cufile.json
 
-test_bin=./biu_check_good
+test_bin=./biu_check_bad
 
 $test_bin
