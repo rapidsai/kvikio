de4bccdfc6c7e80025d77b1a97bf1e815de475c5
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-05-30 09:32:55 -0400

    Add an overload getenv_or that supports env var alias. Add new env var KVIKIO_NUM_THREADS. Fix UB. (#735)
    This PR performs the following items:
    - Add a new overload `kvikio::getenv_or` that supports env var alias, i.e. multiple different env vars referring to the same property.
    - Add new env var `KVIKIO_NUM_THREADS` as the alias of `KVIKIO_NTHREADS`, and add new C++ API `num_threads`/`set_thread_pool_nthreads` as the alias of existing `thread_pool_nthreads`/`set_thread_pool_nthreads`. This makes C++ and Python API (i.e. `kvikio.defaults.get("num_threads")`) consistent.
    - Fix a well-hidden UB related to string temporaries and `std::initializer_list`.
    
    Authors:
      - Tianyu Liu (https://github.com/kingcrimsontianyu)
    
    Approvers:
      - Vukasin Milovanovic (https://github.com/vuule)
      - Mads R. B. Kristensen (https://github.com/madsbk)
    
    URL: https://github.com/rapidsai/kvikio/pull/735

--- cpp/include/kvikio/defaults.hpp
+++ cpp/include/kvikio/defaults.hpp
@@ -18,11 +18,14 @@
 
 #include <cstddef>
 #include <cstdlib>
+#include <initializer_list>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <type_traits>
 
 #include <kvikio/compat_mode.hpp>
+#include <kvikio/error.hpp>
 #include <kvikio/http_status_codes.hpp>
 #include <kvikio/shim/cufile.hpp>
 #include <kvikio/threadpool_wrapper.hpp>
@@ -41,9 +44,13 @@ T getenv_or(std::string_view env_var_name, T default_val)
   std::stringstream sstream(env_val);
   T converted_val;
   sstream >> converted_val;
-  KVIKIO_EXPECT(!sstream.fail(),
-                "unknown config value " + std::string{env_var_name} + "=" + std::string{env_val},
-                std::invalid_argument);
+
+  if constexpr (!std::is_same_v<T, std::string>) {
+    KVIKIO_EXPECT(!sstream.fail(),
+                  "unknown config value " + std::string{env_var_name} + "=" + std::string{env_val},
+                  std::invalid_argument);
+  }
+
   return converted_val;
 }
 
@@ -55,6 +62,59 @@ CompatMode getenv_or(std::string_view env_var_name, CompatMode default_val);
 
 template <>
 std::vector<int> getenv_or(std::string_view env_var_name, std::vector<int> default_val);
+
+/**
+ * @brief Get the environment variable value from a candidate list
+ *
+ * @tparam T Type of the environment variable value
+ * @param env_var_names Candidate list containing the names of environment variable
+ * @param default_val Default value of the environment variable, if none of the candidates has been
+ * found
+ * @return A tuple of (`env_var_name`, `result`, `has_found`), where:
+ *   - If the environment variable is not set by any of the candidates, `has_found` will be false,
+ * `result` will be `default_val`, and `env_var_name` will be empty.
+ *   - If the environment variable is set by `env_var_name`, then `has_found` will be true, and
+ * `result` be the set value. If more than one candidates have been set with the same value,
+ * `env_var_name` will be assigned the last candidate.
+ *
+ * @throws std::invalid_argument if:
+ *   - `env_var_names` is empty.
+ *   - The environment variable is not defined to be string type and is assigned an empty value (in
+ *     other words, string-type environment variables are allowed to hold an empty value).
+ *   - More than one candidates have been set with different values.
+ *   - An invalid value is given, e.g. value that cannot be converted to type T.
+ */
+template <typename T>
+std::tuple<std::string_view, T, bool> getenv_or(
+  std::initializer_list<std::string_view> env_var_names, T default_val)
+{
+  KVIKIO_EXPECT(env_var_names.size() > 0,
+                "`env_var_names` must contain at least one environment variable name.",
+                std::invalid_argument);
+  std::string_view env_name_target;
+  std::string_view env_val_target;
+
+  for (auto const& env_var_name : env_var_names) {
+    auto const* env_val = std::getenv(env_var_name.data());
+    if (env_val == nullptr) { continue; }
+
+    if (!env_name_target.empty() && env_val_target != env_val) {
+      std::stringstream ss;
+      ss << "Environment variable " << env_var_name << " (" << env_val
+         << ") has already been set by its alias " << env_name_target << " (" << env_val_target
+         << ") with a different value.";
+      KVIKIO_FAIL(ss.str(), std::invalid_argument);
+    }
+
+    env_name_target = env_var_name;
+    env_val_target  = env_val;
+  }
+
+  if (env_name_target.empty()) { return {env_name_target, default_val, false}; }
+
+  auto res = getenv_or<T>(env_name_target, default_val);
+  return {env_name_target, res, true};
+}
 
 /**
  * @brief Singleton class of default values used throughout KvikIO.
@@ -182,6 +242,20 @@ class defaults {
    * @param nthreads The number of threads to use.
    */
   static void set_thread_pool_nthreads(unsigned int nthreads);
+
+  /**
+   * @brief Alias of `thread_pool_nthreads`
+   *
+   * @return The number of threads
+   */
+  [[nodiscard]] static unsigned int num_threads();
+
+  /**
+   * @brief Alias of `set_thread_pool_nthreads`
+   *
+   * @param nthreads The number of threads to use
+   */
+  static void set_num_threads(unsigned int nthreads);
 
   /**
    * @brief Get the default task size used for parallel IO operations.
