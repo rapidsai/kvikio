a1bd3eaed2297d615d8377a5ae8c99cebcf2c033
Vyas Ramasubramani <vyasr@nvidia.com>
2025-07-25 11:38:49 -0700

    Revert "Set compiler versions in context (#755)" (#784)
    Closes https://github.com/rapidsai/kvikio/issues/773
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/kvikio/pull/784

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -26,13 +26,33 @@ export RAPIDS_ARTIFACTS_DIR
 source rapids-rattler-channel-string
 
 # Construct the extra variants according to the architecture
-cat > variants.yaml << EOF
-cuda_version:
-  - ${RAPIDS_CUDA_VERSION%.*}
+if [[ "$(arch)" == "x86_64" ]]; then
+    cat > variants.yaml << EOF
+    c_compiler_version:
+      - 14
+
+    cxx_compiler_version:
+      - 14
+
+    cuda_version:
+      - ${RAPIDS_CUDA_VERSION%.*}
 EOF
-if [[ "$(arch)" == "aarch64" ]]; then
-  cat >> variants.yaml << EOF
-  - 12.1 # The last version to not support cufile
+else
+    cat > variants.yaml << EOF
+    zip_keys:
+    - [c_compiler_version, cxx_compiler_version, cuda_version]
+
+    c_compiler_version:
+    - 12
+    - 14
+
+    cxx_compiler_version:
+    - 12
+    - 14
+
+    cuda_version:
+    - 12.1 # The last version to not support cufile
+    - ${RAPIDS_CUDA_VERSION%.*}
 EOF
 fi
 
