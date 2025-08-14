258bf63fcad5faec647dae195c4ba15eee767f50
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-08-14 00:12:43 -0400

    Update

--- python/kvikio/tests/test_hdfs_io.py
+++ python/kvikio/tests/test_hdfs_io.py
@@ -2,12 +2,14 @@
 # See file LICENSE for terms.
 
 
+import json
 import re
 from concurrent.futures import ProcessPoolExecutor
 
-import cupy as cp
+import numpy as np
 import pytest
 from pytest_httpserver import HTTPServer
+from werkzeug import Request, Response
 
 from kvikio import remote_file
 
@@ -16,25 +18,33 @@ class RemoteFileData:
     def __init__(self, file_path, num_elements):
         self.file_path = file_path
         self.num_elements = num_elements
-        self.buf = cp.arange(0, self.num_elements, dtype=cp.float64)
+        self.buf = np.arange(0, self.num_elements, dtype=np.float64)
         self.file_size = self.buf.nbytes
 
 
-def helper_get_file_size(url):
-    handle = remote_file.RemoteFile.open_webhdfs(url)
-    return handle.nbytes()
+remote_file_data = RemoteFileData("/home/test_user/test_file.bin", 1024 * 1024)
+
+
+def webhdfs_server_handle_request(request: Request) -> Response:
+    # request.args is a dictionary of parsed query strings
+    # If client requests the file size
+    if request.args["op"] == "GETFILESTATUS":
+        return Response(
+            response=json.dumps({"length": remote_file_data.file_size}),
+            status=200,
+            content_type="application/json",
+        )
+    if request.args["op"] == "OPEN":
+        begin_idx = int(request.args["offset"])
+        end_idx = begin_idx + int(request.args["length"])
+        range_data = remote_file_data.buf[begin_idx:end_idx]
+        return Response(
+            response=range_data, status=200, content_type="application/octet-stream"
+        )
 
 
 @pytest.fixture
-def remote_file_data() -> RemoteFileData:
-    remote_file_data = RemoteFileData("/home/test_user/test_file.bin", 1024 * 1024)
-    return remote_file_data
-
-
-@pytest.fixture
-def good_server_for_file_size(
-    httpserver: HTTPServer, remote_file_data: RemoteFileData
-) -> HTTPServer:
+def mock_webhdfs_server(httpserver: HTTPServer) -> HTTPServer:
     # Regex meaning:
     # \? Matches the literal question mark
     # [^?]+ Matches the character that is not a question mark one or more times
@@ -42,22 +52,19 @@ def good_server_for_file_size(
     url_pattern = rf"/webhdfs/v1{remote_file_data.file_path}(\?[^?]+)?"
     httpserver.expect_request(
         re.compile(url_pattern),
-        method="GET",
-        query_string={"op": "GETFILESTATUS"},
-    ).respond_with_json(
-        response_json={"length": remote_file_data.file_size}, status=200
-    )
+    ).respond_with_handler(webhdfs_server_handle_request)
 
     return httpserver
 
 
-def test_webhdfs_get_file_size(
-    good_server_for_file_size: HTTPServer, remote_file_data: RemoteFileData
-):
+def helper_get_file_size(url):
+    handle = remote_file.RemoteFile.open_webhdfs(url)
+    return handle.nbytes()
+
+
+def test_webhdfs_get_file_size(mock_webhdfs_server: HTTPServer):
     # Given the file path, url_for prepends the scheme, host and port
-    basic_url = good_server_for_file_size.url_for(
-        f"/webhdfs/v1{remote_file_data.file_path}"
-    )
+    basic_url = mock_webhdfs_server.url_for(f"/webhdfs/v1{remote_file_data.file_path}")
 
     url_list = [basic_url, f"{basic_url}?op=OPEN"]
 
@@ -68,10 +75,26 @@ def test_webhdfs_get_file_size(
             assert file_size == remote_file_data.file_size
 
 
+def helper_read_parallel(url, num_elements):
+    handle = remote_file.RemoteFile.open_webhdfs(url)
+    result_buf = np.arange(0, num_elements, dtype=np.float64)
+    fut = handle.pread(result_buf)
+    read_size = fut.get()
+    return read_size, result_buf
+
+
+def test_webhdfs_read_parallel(mock_webhdfs_server: HTTPServer):
+    url = mock_webhdfs_server.url_for(f"/webhdfs/v1{remote_file_data.file_path}")
+
+    with ProcessPoolExecutor() as executor:
+        fut = executor.submit(helper_read_parallel, url, remote_file_data.num_elements)
+        read_size, result_buf = fut.result()
+        assert read_size == remote_file_data.file_size
+        assert np.array_equal(result_buf, remote_file_data.buf)
+
+
 @pytest.fixture
-def bad_server_for_file_size(
-    httpserver: HTTPServer, remote_file_data: RemoteFileData
-) -> HTTPServer:
+def mock_bad_server(httpserver: HTTPServer) -> HTTPServer:
     httpserver.expect_request(
         f"/webhdfs/v1{remote_file_data.file_path}",
         method="GET",
@@ -83,10 +106,8 @@ def bad_server_for_file_size(
     return httpserver
 
 
-def test_webhdfs_bad_server(
-    bad_server_for_file_size: HTTPServer, remote_file_data: RemoteFileData
-):
-    url = bad_server_for_file_size.url_for(f"/webhdfs/v1{remote_file_data.file_path}")
+def test_webhdfs_missing_file_size(mock_bad_server: HTTPServer):
+    url = mock_bad_server.url_for(f"/webhdfs/v1{remote_file_data.file_path}")
 
     with pytest.raises(
         RuntimeError,
@@ -96,60 +117,3 @@ def test_webhdfs_bad_server(
         with ProcessPoolExecutor() as executor:
             fut = executor.submit(helper_get_file_size, url)
             fut.result()
-
-
-# @pytest.fixture
-# def server_for_read(httpserver: HTTPServer,
-#                     remote_file_data: RemoteFileData) -> HTTPServer:
-#     # Optional HEAD some clients issue
-#     # httpserver.expect_request(
-#     #     "/webhdfs/v1/home/user/test.bin",
-#     #     method="HEAD",
-#     #     query_string={"op": "OPEN", "user.name": "testuser"},
-#     # ).respond_with_data(b"", status=200)
-
-#     # # Coordinator GET: send 307 redirect with Location
-#     # redirect_target_path = "/webhdfs/v1/home/user/test.bin"
-#     # redirect_qs = "op=OPEN&user.name=testuser&datanode=true"
-#     # redirect_url = httpserver.url_for(redirect_target_path) + "?" + redirect_qs
-
-#     # httpserver.expect_request(
-#     #     redirect_target_path,
-#     #     method="GET",
-#     #     query_string={"op": "OPEN", "user.name": "testuser"},
-#     # ).respond_with_data(
-#     #     b"", status=307, headers={"Location": redirect_url}
-#     # )
-
-#     # Regex meaning:
-#     # \? Matches the literal question mark
-#     # [^?]+ Matches the character that is not a question mark one or more times
-#     # ()? Matches the query zero or one time
-#     url_pattern = fr"/webhdfs/v1{remote_file_data.file_path}(\?[^?]+)?"
-#     httpserver.expect_request(re.compile(url_pattern),
-#                               method="GET",
-#                               query_string={"op": "GETFILESTATUS"},
-#                               ).respond_with_json(
-#         response_json={"length": remote_file_data.file_size},
-#         status=200)
-
-#     return httpserver
-
-
-# def helper_read_parallel(url, num_elements):
-#     handle = remote_file.RemoteFile.open_webhdfs(url)
-#     buf = cp.arange(0, num_elements, dtype=cp.float64)
-#     fut = handle.pread(buf)
-#     read_size = fut.get()
-#     return read_size
-
-
-# def test_webhdfs_read_parallel(server_for_read: HTTPServer,
-#                                remote_file_data: RemoteFileData):
-#     url = server_for_read.url_for(f"/webhdfs/v1{remote_file_data.file_path}")
-
-#     with ProcessPoolExecutor() as executor:
-#         fut = executor.submit(helper_read_parallel, url,
-#                               remote_file_data.num_elements)
-#         read_size = fut.result()
-#         assert read_size == remote_file_data.file_size
