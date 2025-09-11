e1fee9e070dcb1927a2792e97eb23c467ecff54e
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-09-11 12:09:24 -0400

    Update

--- cpp/tests/test_tls.cpp
+++ cpp/tests/test_tls.cpp
@@ -26,7 +26,10 @@ TEST(TlsTest, get_ca_paths)
   std::string const expected_ca_bundle_path{"ca_bundle_path"};
   std::string const expected_ca_directory{"ca_directory"};
   {
+    // Env var CURL_CA_BUNDLE has the highest priority. Both SSL_CERT_FILE and SSL_CERT_DIR shall be
+    // skipped
     kvikio::test::EnvVarContext env_var_ctx{{"CURL_CA_BUNDLE", expected_ca_bundle_path},
+                                            {"SSL_CERT_FILE", "another_ca_bundle_path"},
                                             {"SSL_CERT_DIR", expected_ca_directory}};
     auto const& [ca_bundle_file, ca_directory] = kvikio::detail::get_ca_paths();
 
@@ -35,6 +38,7 @@ TEST(TlsTest, get_ca_paths)
   }
 
   {
+    // Env var CURL_CA_BUNDLE and SSL_CERT_FILE are not specified, SSL_CERT_DIR shall be used
     kvikio::test::EnvVarContext env_var_ctx{{"SSL_CERT_DIR", expected_ca_directory}};
     auto const& [ca_bundle_file, ca_directory] = kvikio::detail::get_ca_paths();
 
