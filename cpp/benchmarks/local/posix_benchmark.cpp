f65776cc41f392f3ce9d26ba0f5d5c6d6c491e44
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-11-18 15:54:58 -0500

    Add seq read benchmark

---
+++ cpp/benchmarks/local/posix_benchmark.cpp
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2025, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kvikio/defaults.hpp>
+
+int main() { return 0; }
