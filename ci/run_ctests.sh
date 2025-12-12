84ab76ca85ef41b45da856e6f48a253c8289c6c0
Bradley Dice <bdice@bradleydice.com>
2025-12-12 03:48:32 +0000

    Fix paths for examples

--- ci/run_ctests.sh
+++ ci/run_ctests.sh
@@ -12,8 +12,12 @@ devcontainers_test_location="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../cpp
 
 if [[ -d "${installed_test_location}" ]]; then
     cd "${installed_test_location}"
+    # Conda packages install binaries directly in this directory
+    examples_dir="."
 elif [[ -d "${devcontainers_test_location}" ]]; then
     cd "${devcontainers_test_location}"
+    # Devcontainer builds install binaries in an examples subdirectory
+    examples_dir="examples"
 else
     echo "Error: Test location not found. Searched:" >&2
     echo "  - ${installed_test_location}" >&2
@@ -22,8 +26,8 @@ else
 fi
 
 # Run basic tests
-./BASIC_IO_EXAMPLE
-./BASIC_NO_CUDA_EXAMPLE
+${examples_dir}/BASIC_IO_EXAMPLE
+${examples_dir}/BASIC_NO_CUDA_EXAMPLE
 
 # Run gtests
 ctest --no-tests=error --output-on-failure "$@"
