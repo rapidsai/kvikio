3f656e18b5784cc71b7b5713d6d843de7efd5642
Gil Forsyth <gforsyth@nvidia.com>
2026-01-05 15:25:43 -0500

    feat(sabi): build Python wheels with limited API

--- ci/build_wheel_python.sh
+++ ci/build_wheel_python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
@@ -26,3 +26,9 @@ python -m auditwheel repair \
     ${package_dir}/dist/*
 
 ./ci/validate_wheel.sh ${package_dir} "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+# Only use stable ABI package naming for Python >= 3.11
+if [[ "${RAPIDS_PY_VERSION}" != "3.10" ]]; then
+  RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python kvikio --stable --cuda)"
+  export RAPIDS_PACKAGE_NAME
+fi
