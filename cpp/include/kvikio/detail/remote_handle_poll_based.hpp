8d976b235c3fd07c0f041321772bc32c780db34a
Tianyu Liu <kingcrimsontianyu@gmail.com>
2026-01-12 16:00:44 -0500

    Update

--- cpp/include/kvikio/detail/remote_handle_poll_based.hpp
+++ cpp/include/kvikio/detail/remote_handle_poll_based.hpp
@@ -8,16 +8,31 @@
 
 #include <kvikio/shim/libcurl.hpp>
 
+#define KVIKIO_CHECK_CURL_MULTI(err_code) \
+  kvikio::detail::check_curl_multi(err_code, __FILE__, __LINE__)
+
 namespace kvikio::detail {
+
+inline void check_curl_multi(CURLMcode err_code, char const* filename, int line_number)
+{
+  if (err_code == CURLMcode::CURLM_OK) { return; }
+  std::stringstream ss;
+  ss << "libcurl error: " << curl_multi_strerror(err_code) << " at: " << filename << ":"
+     << line_number << "\n";
+  throw std::runtime_error(ss.str());
+}
 
 class RemoteHandlePollBased {
  public:
   RemoteHandlePollBased(std::string const& url);
+
+  ~RemoteHandlePollBased();
 
   std::size_t pread(void* buf, std::size_t size, std::size_t file_offset = 0);
 
  private:
   CURLM* _multi;
   std::string _url;
+  std::size_t _num_conns{8};
 };
 }  // namespace kvikio::detail
--- cpp/src/detail/remote_handle_poll_based.cpp
+++ cpp/src/detail/remote_handle_poll_based.cpp
@@ -4,9 +4,24 @@
  */
 
 #include <kvikio/detail/remote_handle_poll_based.hpp>
+#include <kvikio/error.hpp>
 
 namespace kvikio::detail {
-RemoteHandlePollBased::RemoteHandlePollBased(std::string const& url) {}
+
+RemoteHandlePollBased::RemoteHandlePollBased(std::string const& url) : _url{url}
+{
+  _multi = curl_multi_init();
+  KVIKIO_EXPECT(_multi != nullptr, "Failed to initialize libcurl multi API");
+}
+
+RemoteHandlePollBased::~RemoteHandlePollBased()
+{
+  try {
+    KVIKIO_CHECK_CURL_MULTI(curl_multi_cleanup(_multi));
+  } catch (std::exception const& e) {
+    KVIKIO_LOG_ERROR(e.what());
+  }
+}
 
 std::size_t RemoteHandlePollBased::pread(void* buf, std::size_t size, std::size_t file_offset)
 {
