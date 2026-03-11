765f4934158c7565a3c12fb0afa47f958a4524c1
Tianyu Liu <kingcrimsontianyu@gmail.com>
2026-03-11 00:05:52 -0400

    Corrected typo

--- cpp/include/kvikio/detail/posix_io.hpp
+++ cpp/include/kvikio/detail/posix_io.hpp
@@ -204,7 +204,7 @@ ssize_t posix_host_io(
  * @param fd_direct_on File descriptor opened with O_DIRECT, or -1 to disable Direct I/O attempts
  * @return Total number of bytes read or written
  *
- * @note For reads, `file_offset + count` must not exceed the file size. Reading past EOF is not
+ * @note For reads, `file_offset + size` must not exceed the file size. Reading past EOF is not
  * supported and will result in an error.
  */
 template <IOOperationType Operation, typename BounceBufferPoolType = CudaPinnedBounceBufferPool>
