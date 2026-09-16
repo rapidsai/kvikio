1ade96e533bf0fd31760462dcd7e8d2d55fa5152
Kyle Edwards <kyedwards@nvidia.com>
2026-09-16 11:47:17 -0400

    Copyright

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # kvikio build script
